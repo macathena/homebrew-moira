cb68f78f2e30278f2489db5a6e34e9c6d67675d4
E. Lee <leee@mit.edu>
2020-03-14 09:41:01 -0400

    Provide moira-dev formula
    Signed-off-by: E. Lee <leee@mit.edu>

--- moira-dev.rb
+++ moira-dev.rb
@@ -1,13 +1,13 @@
-class Moira < Formula
-  desc "Clients for the Athena Service Management System"
-  homepage "https://debathena.mit.edu"
-  url "https://github.com/mit-athena/moira/archive/4.0.0.3+51+g65d55c5.tar.gz"
-  sha256 "d418681ae4ec61a124c55fb67a6bba0d89b59dc760c4d1c9a1e8e1c9c7b69b19"
+class MoiraDev < Formula
+  desc "Clients for the Athena Service Management System (developer)"
+  homepage "https://github.mit.edu/ops/moira"
+  url "git@github.mit.edu:ops/moira.git", :using => :git
+  version "0"
 
   depends_on :macos => :el_capitan
   depends_on "hesiod"
   depends_on "krb5"
-  conflicts_with "moira-dev", :because => "moira-dev is moira"
+  conflicts_with "moira", :because => "moira-dev is moira"
 
   def install
     args = %W[
